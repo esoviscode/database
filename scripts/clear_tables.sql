330f4f03b85d155d3fb96423b2ef91b3fdf469de
Cellmer <michal.cellmer@gmail.com>
2023-05-17 12:46:59 +0200

    adjust drop_tables and clear_tables scripts to new db

--- scripts/clear_tables.sql
+++ scripts/clear_tables.sql
@@ -5,7 +5,7 @@ TRUNCATE public."Entity" CASCADE;
 TRUNCATE public."Creature" CASCADE;
 TRUNCATE public."Dialog" CASCADE;
 TRUNCATE public."Item" CASCADE;
-TRUNCATE public."Creature_Item" CASCADE;
+TRUNCATE public."Player_Item" CASCADE;
 TRUNCATE public."Skill" CASCADE;
 TRUNCATE public."Entity_Skill" CASCADE;
 TRUNCATE public."Equipment" CASCADE;
